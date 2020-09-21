4e616c36f732b450d5215717197e986c1dd53b45
overlay0110 <71483442+overlay0110@users.noreply.github.com>
2020-09-21 09:57:18 +0900

    Update config.js

--- src/main/import/config.js
+++ src/main/import/config.js
@@ -1,22 +1,9 @@
 const config = {
-    // URL : 'http://192.168.1.18/test/test.php',
-    // END_PAGE : ['test.php'],
-    // ROOT_CHECK : true,
-    // PIN : true,
-    // PIN_SET : {
-    //     numberOfPins : 6,
-    //     keyVibration : true,
-    //     backGroundColor : '#207BF7',
-    // },
-    // LOCATION : true,
-    // QRSCAN : true,
-
     APP_LOADING_PAGE : 'http://192.168.1.18/test/loading.php',
     CONFIG_URL : 'http://192.168.1.18/moblie/work/appConfigs/',
     CONFIG_FILE_NAME : 'appBaseConfig.json',
 
     DEBUG : false,
-    // STATUS_BAR_COLOR : '#207BF7',
     STATUS_BAR_COLOR : 'gray',
 }
 
